7fcac88e62fd3160ff7a5bd5ab380a79a4e643ac
Michel Weststrate <mweststrate@gmail.com>
2018-12-18 11:52:45 +0100

    x

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@ const child_process = require("child_process")
 const os = require("os")
 
 const binFolder = child_process.execSync("yarn bin")
-const microbundle = binFolder + "/microbundle"
+const microbundle = binFolder.trim() + "/microbundle"
 
 const projects = ["core", "react", "immer", "models"]
 const externals = ["immer", "react"]
