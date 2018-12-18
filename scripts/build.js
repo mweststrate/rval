5d0a2e1850b601e6f92954c36f34663ff5eac146
Michel Weststrate <mweststrate@gmail.com>
2018-12-18 04:48:59 +0100

    Build improvements

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,5 @@
 const fs = require("fs")
-const path = require("path")
 const child_process = require("child_process")
-
-// exit upon first error
-shell.set("-e")
 
 const projects = ["core", "react", "immer", "models"]
 const externals = ["immer", "react"]
@@ -13,7 +9,7 @@ const packageJson = fs.readFileSync("pkgs/package-template.json", "utf8")
 const buildCommand = "..\\..\\node_modules\\.bin\\microbundle.cmd --entry $pkg.ts --name rval$pkg --strict --format es,cjs,umd --globals "
    + [].concat(projects.map(pkg => "@rval/" + pkg + "=rval" + pkg), externals.map(pkg => pkg + "=" + pkg)).join(",") + ",immer=immer,react=react --external "
    + externals.concat(projects.map(pkg => "@rval/" + pkg)).join(",")
-   + " && mv dist/$pkg/*.d.ts dist/ && rimraf dist/$pkg"
+   + " && mv dist/$pkg/*.d.ts dist/ && rimraf dist/$pkg .rts2*"
 
 projects.forEach(pkg => {
     fs.writeFileSync("pkgs/" + pkg + "/package.json", packageJson.replace(/\$pkg/g, pkg), "utf8")
