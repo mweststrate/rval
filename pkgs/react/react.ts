0d33d897cd1f5fef2d3a144aa4c13cecaa69e4ac
Michel Weststrate <mweststrate@gmail.com>
2018-12-18 04:49:12 +0100

    introduced `useLocalVal`, `useLocalDrv`

--- pkgs/react/react.ts
+++ pkgs/react/react.ts
@@ -1,20 +1,25 @@
-import { Observable, isVal, isDrv, defaultContext, rval, RValFactories } from '@rval/core'
+import { Observable, isVal, isDrv, defaultContext, rval, RValFactories, Val, Drv } from '@rval/core'
 import { useState, useEffect, useMemo, ReactNode, ReactElement, createElement } from 'react'
 
 export function useVal<T>(observable: Observable<T>): T {
   if (!isVal(observable) && !isDrv(observable)) throw new Error('useval - expected val or drv')
-  let f // forward ref to the updater function, so that we can subscribe first
-  // this implementation could be simpler if we would first used `useState`,
-  // and then set up the subscription in useEffect.
-  // The benefit of this setup, is that we set up the subscription first,
-  // so that observable is already 'hot' before we read it for the first time
-  // See: test "useVal - mimimum computations - 2"
-  // Note: `rval(sub)` makes sure we use the context of the observable passed in
-  const disposer = useMemo(() => rval(observable).sub(observable, x => f(x)), [observable])
-  const [val, updater] = useState(observable) // short-cut for initializer with fn: actually: () => observable()
-  f = updater
+  const [_, setTick] = useState(0)
+  const disposer = useMemo(() => 
+    rval(observable).sub(observable, () => setTick(tick => tick + 1))
+  , [observable])
   useEffect(() => disposer, [observable])
-  return val
+  return observable()
+}
+
+export function useLocalVal<T>(initial: T, rvalContext = defaultContext): [T, Val<T>] {
+  const val = useMemo(() => rvalContext.val(initial), [])
+  const current = useVal(val)
+  return [current, val]
+}
+
+export function useLocalDrv<T>(derivation: () => T, inputs: any[] = [], rvalContext = defaultContext): T {
+  const drv = useMemo(() => rvalContext.drv(derivation), inputs)
+  return useVal(drv)
 }
 
 export function rview(render: () => ReactNode, rvalContext = defaultContext, inputs?: any[]): ReactElement<any> | null {
--- pkgs/react/tests/useval.spec.tsx
+++ pkgs/react/tests/useval.spec.tsx
@@ -1,5 +1,5 @@
 import { val, drv, rval } from '@rval/core'
-import { useVal } from '@rval/react'
+import { useVal, useLocalVal, useLocalDrv } from '@rval/react'
 import * as React from 'react'
 import { render, waitForElement } from 'react-testing-library'
 
@@ -95,4 +95,67 @@ test('useVal - custom rval ', async () => {
 
   counter(counter() + 1)
   await waitForElement(() => re.container.innerHTML === '<h1>2</h1>')
+})
+
+test('useLocalVal - 1 ', async () => {
+  let updater
+  const Comp = () => {
+    const [counter, u] = useLocalVal(0)
+    updater = u
+    return <h1>{counter}</h1>
+  }
+
+  const re = render(<Comp />)
+  expect(re.container.innerHTML).toEqual('<h1>0</h1>')
+
+  updater(updater() + 1)
+  await waitForElement(() => re.container.innerHTML === '<h1>1</h1>')
+
+  updater(updater() + 1)
+  await waitForElement(() => re.container.innerHTML === '<h1>2</h1>')
+})
+
+test('useLocalDrv - 1 ', async () => {
+  const counter = val(0)
+  const Comp = () => {
+    const doubled = useLocalDrv(() => 2 * counter())
+    return <h1>{doubled}</h1>
+  }
+
+  const re = render(<Comp />)
+  expect(re.container.innerHTML).toEqual('<h1>0</h1>')
+
+  counter(counter() + 1)
+  await waitForElement(() => re.container.innerHTML === '<h1>2</h1>')
+
+  counter(counter() + 1)
+  await waitForElement(() => re.container.innerHTML === '<h1>4</h1>')
+})
+
+
+test('useLocalDrv - with state & inputs ', async () => {
+  const counter = val(1)
+  let updater
+  const Comp = () => {
+    const [multiplier, setM] = React.useState(2)
+    updater = setM
+    const doubled = useLocalDrv(() => multiplier * counter(), [multiplier])
+    return <h1>{doubled}</h1>
+  }
+
+  const re = render(<Comp />)
+  expect(re.container.innerHTML).toEqual('<h1>2</h1>')
+
+  updater(3)
+  delay(10)
+  expect(re.container.innerHTML).toEqual('<h1>3</h1>')
+
+  counter(counter() + 1)
+  delay(10)
+  expect(re.container.innerHTML).toEqual('<h1>6</h1>')
+
+  updater(1)
+  delay(10)
+  expect(re.container.innerHTML).toEqual('<h1>2</h1>')
+
 })
