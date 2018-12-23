e37fdb0b0b81cc6ea7be87c70602953ae0ae3b54
Michel Weststrate <mweststrate@gmail.com>
2018-12-23 02:27:36 +0100

    Fixed base, and build setup

---
+++ doczrc.js
@@ -0,0 +1,17 @@
+export default {
+    // menu: [
+    //     {
+    //         name: "Introduction"           
+    //     }, 
+    //     {
+    //         name: "Advanced"
+    //     }, 
+    //     {
+    //         name: "API"
+    //     }
+    // ],
+    base: '/rval/',
+    dest: 'docs',
+    propsParser: false,
+    hashRouter: true
+}
